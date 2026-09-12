f7b6194cbae3e4f8f2b0bb26aeb8e2745001fdb9
knj050728 <knj050728@korea.ac.kr>
2026-09-12 20:34:42 +0900

    Change CORS middleware to allow all origins

--- app/main.py
+++ app/main.py
@@ -17,7 +17,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[o for o in origins if o],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
